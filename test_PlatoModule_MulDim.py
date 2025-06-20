bf4de66d6ce96a050e6043da057d9464d76002b5
Dmitry <gordeev.dmitriy@physics.msu.ru>
2025-06-20 21:07:54 +0300

    MarkPlatoRegions

--- test_PlatoModule_MulDim.py
+++ test_PlatoModule_MulDim.py
@@ -22,3 +22,22 @@ class TestPlatoModule_MulDim(TestCase):
         self.assertEqual(opt.plato_module.plato_indexes[0][0], 2)
         self.assertEqual(opt.plato_module.plato_indexes[0][1], 5)
         self.assertTrue(opt.known_values.iloc[4]["plato_block"])
+
+
+
+    # ================================================================================
+    # MarkPlatoRegions()
+    def test_MarkPlatoRegions(self):
+        opt = CreateOptimizer_Instance("test_table5.csv")
+        opt.plato_module.plato_indexes = [(1, 2), (4, 5)]
+        opt.plato_module.MarkPlatoRegions()
+
+        plato_indexes = opt.known_values["plato_index"].to_list()
+        plato_edge = opt.known_values["plato_edge"].to_list()
+
+        gt_plato_indexes = [-1, 0, 0, 0, 1, 1, 1, -1]
+        gt_plato_edge    = [False, True, False, True, True, False, True, False]
+
+        for i in range(opt.known_values.shape[0]):
+            self.assertEqual(gt_plato_indexes[i], plato_indexes[i])
+            self.assertEqual(gt_plato_edge[i], plato_edge[i])
