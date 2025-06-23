fa7c4540a82b5471e876dd30bb1d1856de0cdfdc
Dmitry <gordeev.dmitriy@physics.msu.ru>
2025-06-23 21:47:55 +0300

    rm git index

--- test_Functional.py
+++ test_Functional.py
@@ -4,7 +4,7 @@ from test_helper import *
 
 class TestOptimizer_FunctionalRuns(TestCase):
     def test_functional_runs(self):
-        opt = MulDimOptimizer(decay)
+        opt = MulDimOptimizer(sombrero)
         opt.Init(names=["x1", "x2"], mins=[-10, -10], maxs=[10, 10])
         opt.SetupEps(block_eps=0.01, plato_block_eps=0.05, plato_x_eps=4.0, plato_y_eps=0.1)
         opt.RunCycle(10)
